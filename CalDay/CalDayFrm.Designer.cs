4ea5b2f361a15d4a84f99fa934dd5db0c1125d55
Zhifeng Lv <lvzhifeng2013@163.com>
2019-12-28 16:38:07 +0800

    Add files via upload

---
+++ CalDay/CalDayFrm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CalDay
+{
+    partial class CalDayFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt10000Day = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotalDays = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(115, 21);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(164, 21);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "出生日期：";
+            // 
+            // txt10000Day
+            // 
+            this.txt10000Day.Location = new System.Drawing.Point(115, 101);
+            this.txt10000Day.Name = "txt10000Day";
+            this.txt10000Day.Size = new System.Drawing.Size(164, 21);
+            this.txt10000Day.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "出生10000天：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "出生天数：";
+            // 
+            // txtTotalDays
+            // 
+            this.txtTotalDays.Location = new System.Drawing.Point(115, 62);
+            this.txtTotalDays.Name = "txtTotalDays";
+            this.txtTotalDays.Size = new System.Drawing.Size(164, 21);
+            this.txtTotalDays.TabIndex = 5;
+            // 
+            // CalDayFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 146);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTotalDays);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt10000Day);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.MaximizeBox = false;
+            this.Name = "CalDayFrm";
+            this.Text = "计算天数";
+            this.Load += new System.EventHandler(this.CalDayFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt10000Day;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTotalDays;
+    }
+}
+
---
+++ CalDay/CalDayFrm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalDay
+{
+    public partial class CalDayFrm : Form
+    {
+        public CalDayFrm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CalDayFrm_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = new DateTime(1994,06,06);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime birthday = dateTimePicker1.Value;    
+            txt10000Day.Text = CalLiveDay(birthday).ToString();      
+            txtTotalDays.Text = Get10000DayDate(birthday).ToString();
+        }
+
+        int CalLiveDay(DateTime birthday)
+        {
+            DateTime endTime = DateTime.Now;
+            TimeSpan timeSpan = endTime - birthday;
+            return timeSpan.Days;
+        }
+
+        DateTime Get10000DayDate(DateTime birthday)
+        {
+            DateTime startTime = birthday;
+            DateTime desinationTime = startTime.AddDays(10000);
+            return desinationTime;
+        }
+    }
+}
